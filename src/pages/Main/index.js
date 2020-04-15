97bccd43db80d9f71b04808ceddc5f5790134d6d
ederfmatos <ederfmatos@gmail.com>
2020-04-14 22:34:36 -0300

    Estilizando listagem

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,25 +3,43 @@ import { Keyboard } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import {
+  Container,
+  Form,
+  Input,
+  SubmitButton,
+  List,
+  User,
+  Avatar,
+  ProfileButton,
+  Name,
+  Bio,
+  ProfileButtonText,
+} from './styles';
 
 export default function Main() {
   const [users, setUsers] = useState([]);
   const [newUser, setNewUser] = useState('');
 
-  const handleSubmit = useCallback(async () => {
-    const { data } = await api.get(`/users/${newUser}`);
+  const handleSubmit = useCallback(() => {
+    if (!newUser) return;
 
-    const user = {
-      name: data.name,
-      login: data.login,
-      bio: data.bio,
-      avatar: data.avatar_url,
-    };
+    async function loadUser() {
+      const response = await api.get(`/users/${newUser.trim()}`);
 
-    setUsers([...users, user]);
-    setNewUser('');
-    Keyboard.dismiss();
+      const user = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
+
+      setUsers([...users, user]);
+      setNewUser('');
+      Keyboard.dismiss();
+    }
+
+    loadUser();
   }, []);
 
   return (
@@ -41,6 +59,22 @@ export default function Main() {
           <Icon name="add" size={20} color="#FFF" />
         </SubmitButton>
       </Form>
+
+      <List
+        data={users}
+        keyExtractor={user => user.login}
+        renderItem={({ item }) => (
+          <User>
+            <Avatar source={{ uri: item.avatar }} />
+            <Name>{item.name}</Name>
+            <Bio>{item.bio}</Bio>
+
+            <ProfileButton onPress={() => {}}>
+              <ProfileButtonText>Ver perfil</ProfileButtonText>
+            </ProfileButton>
+          </User>
+        )}
+      />
     </Container>
   );
 }
